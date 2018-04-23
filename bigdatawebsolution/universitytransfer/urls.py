604e305761bebf8f28e035b428a10acf12bf1e48
Amar Sukhu <asukhu@gmail.com>
2018-04-23 11:50:29 +0000

    query dynamodb for course. started the logic to pull back courses using ajax. deleted stale project

--- bigdatawebsolution/universitytransfer/urls.py
+++ bigdatawebsolution/universitytransfer/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from universitytransfer import views
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 
@@ -9,7 +9,8 @@ urlpatterns = [
 	url(r'^$', views.HomePageView.as_view(), name='home'),
 	url(r'dynamicgraph', views.DynamicGraphPageView.as_view(), name='dynamicgraph'),    
 	url(r'^about/$', views.AboutPageView.as_view(), name='about'),
-	url(r'^nhti/$', views.NHTIPageView.as_view(), name='nhti'),
+	re_path(r'^nhti\/([0-9]+\/)?$', views.NHTIPageView.as_view(), name='nhti'),
+    url(r'^ajax/getcourse/$', views.getCourses, name='get_course'),
 	url(r'^mcc/$', views.MCCPageView.as_view(), name='mcc'),
 	url(r'^ncc/$', views.NCCPageView.as_view(), name='ncc'),
 ]
--- classproject/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/omeed/Desktop/classproject/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
